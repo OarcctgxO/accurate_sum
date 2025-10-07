0a6dffa46a7289edc4dd4b038cf59b36b8b8a4f2
OarcctgxO <alexandrtok24@gmail.com>
2025-10-07 17:51:43 +0300

    Поправлена псевдооптимизация - выход из заполнения dp по достижению запрашиваемой суммы. Ранее приводила к ошибкам восстановления суммы.

--- accurate_sum.py
+++ accurate_sum.py
@@ -29,10 +29,6 @@ def accurate_sum(needed: int, nums: list[int])-> tuple[int, list[int]]:
         for i in range(max_sum-1, n-1, -1):
             if dp[i-n]:
                 dp[i] = True
-                if i == needed:
-                    break
-        if dp[needed]:
-            break
     
     #нахождение ближайшей достижимой суммы (сверху)
     for i in range(needed, max_sum):
@@ -43,19 +39,19 @@ def accurate_sum(needed: int, nums: list[int])-> tuple[int, list[int]]:
     #восстановление использованных элементов
     current_sum = final_sum
     final_list = []
-    while current_sum > 0:
-        for n in sorted_nums:
-            if (current_sum - n) >= 0:
-                if dp[current_sum-n]:
-                    final_list.append(n)
-                    current_sum -= n
-            if current_sum == 0:
-                break
+    for n in sorted_nums:
+        if (current_sum - n) >= 0:
+            if dp[current_sum-n]:
+                final_list.append(n)
+                current_sum -= n
+        if current_sum == 0:
+            break
+        
     final_list.sort(reverse=True)
     
     return final_sum, final_list
 
 
 if __name__ == "__main__":
-    l = [5, 5, 6, 2, 3]
+    l = [100, 98, 86, 65, 43, 37, 22, 19, 14]
     print(accurate_sum(int(input(f"Введите число, которое нужно собрать суммой списка {l}\n")), l))
